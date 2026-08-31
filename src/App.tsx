121ad0a80de154593822178bf1e1ac61928f6b0b
Karlos Montoya <karlosmontoya91@gmail.com>
2026-08-30 21:00:11 -0600

    Switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import Packages from './pages/Packages';
-import PlaceholderPage from './pages/PlaceholderPage';
 import Configuracion from './pages/Configuracion';
+import PlaceholderPage from './pages/PlaceholderPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AdminLayout />}>
           <Route index element={<Home />} />
-          <Route path="cotizar" element={<PlaceholderPage title="Nueva Cotización" />} />
-          <Route path="cotizaciones" element={<PlaceholderPage title="Historial de Cotizaciones" />} />
-          <Route path="paquetes" element={<Packages />} />
           <Route path="articulos" element={<Catalog />} />
-          <Route path="precios" element={<PlaceholderPage title="Precios y Servicios Escalonados" />} />
-          <Route path="promociones" element={<PlaceholderPage title="Promociones y Descuentos" />} />
-          <Route path="traslados" element={<PlaceholderPage title="Costos de Traslado" />} />
+          <Route path="paquetes" element={<Packages />} />
+          <Route path="cotizaciones" element={<PlaceholderPage title="Cotizaciones" />} />
+          <Route path="precios" element={<PlaceholderPage title="Precios (Servicios Escalonados)" />} />
+          <Route path="promociones" element={<PlaceholderPage title="Promociones" />} />
+          <Route path="traslados" element={<PlaceholderPage title="Traslados y Zonas" />} />
           <Route path="configuracion" element={<Configuracion />} />
-          
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
